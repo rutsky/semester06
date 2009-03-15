1325dfccff20d9070c8d8e8274947a647e956642
Vladimir Rutsky <altsysrq@gmail.com>
2009-03-15 19:31:34 +0000

    Using FASMARM now.

---
+++ test.asm
@@ -0,0 +1,7 @@
+; test.asm
+; Test block that will be appended to firmware end and runned from some point.
+; Vladimir Rutsky <altsysrq@gmail.com>
+; 15.03.2009
+
+    use32
+        add     r0, r0, r1 
