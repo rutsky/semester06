c570c5da84af9a1413fa75ffe7bea86f3e9f3f4a
Vladimir Rutsky <altsysrq@gmail.com>
2009-03-23 04:08:36 +0300

    Added test for generating data for report.

--- Optimizations_methods/linear_programming_problem/src/main.cpp
+++ Optimizations_methods/linear_programming_problem/src/main.cpp
@@ -23,6 +23,7 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/matrix_proxy.hpp>
 #include <boost/numeric/ublas/io.hpp>
+#include <boost/scoped_ptr.hpp>
 #include <iterator>
 #include <algorithm>
 // end of debug
@@ -35,31 +36,46 @@ int main()
   
   //if (0)
   {
-    std::ifstream is("data/mine");
+    std::ifstream is("../data/mine.in");
     
     vector_type c, b, variablesSigns, constraintsSigns;
     matrix_type A;
     
     is >> c >> b >> A >> variablesSigns >> constraintsSigns;
     
-    std::cout << "c:\n" << c << "\n";
-    std::cout << "b:\n" << b << "\n";
-    std::cout << "A:\n" << A << "\n";
-    std::cout << "variablesSigns:\n" << variablesSigns << "\n";
-    std::cout << "constraintsSigns:\n" << constraintsSigns << "\n";
-
-    std::cout << "'A' rows:\n";
-    std::copy(numeric::matrix_rows_begin(A), numeric::matrix_rows_end(A), 
-      std::ostream_iterator<vector_type>(std::cout, "\n"));
+    {
+      std::cout << "c:\n" << c << "\n";
+      std::cout << "b:\n" << b << "\n";
+      std::cout << "A:\n" << A << "\n";
+      std::cout << "variablesSigns:\n" << variablesSigns << "\n";
+      std::cout << "constraintsSigns:\n" << constraintsSigns << "\n";
+  
+      std::cout << "'A' rows:\n";
+      std::copy(numeric::matrix_rows_begin(A), numeric::matrix_rows_end(A), 
+        std::ostream_iterator<vector_type>(std::cout, "\n"));
+      
+      matrix_type const X(A);
+      std::cout << "Const 'A' rows:\n";
+      std::copy(numeric::matrix_rows_begin(X), numeric::matrix_rows_end(X), 
+        std::ostream_iterator<vector_type>(std::cout, "\n"));
+      
+      vector_type resultV(A.size2());
+      numeric::simplex::simplex_result_type const result = numeric::simplex::solve_augment(A, b, c, resultV);
+      std::cout << "Solution: " << resultV << " (result=" << static_cast<int>(result) << ")\n";
+    }
     
-    matrix_type const X(A);
-    std::cout << "Const 'A' rows:\n";
-    std::copy(numeric::matrix_rows_begin(X), numeric::matrix_rows_end(X), 
-      std::ostream_iterator<vector_type>(std::cout, "\n"));
+    {
+      boost::scoped_ptr<std::ofstream> ofs;
     
-    vector_type resultV(A.size2());
-    numeric::simplex::simplex_result_type const result = numeric::simplex::solve_augment(A, b, c, resultV);
-    std::cout << "Solution: " << resultV << " (result=" << static_cast<int>(result) << ")\n";
+      ofs.reset(new std::ofstream("../report/matrix_A.tex"));
+      numeric::output_matrix_tex(*ofs, A);
+      
+      ofs.reset(new std::ofstream("../report/vector_b.tex"));
+      numeric::output_vector_tex(*ofs, b, false);
+      
+      ofs.reset(new std::ofstream("../report/vector_c.tex"));
+      numeric::output_vector_tex(*ofs, c, true);
+    }
   }
   
   {
@@ -72,6 +88,6 @@ int main()
         std::back_inserter(rows), std::back_inserter(cols));
     */
   }
-
+  
   return 0;
 }
