25441b00f831aacebe46c71d0486a7716e0fc6b2
Vladimir Rutsky <altsysrq@gmail.com>
2009-03-29 18:32:12 +0400

    Added source template for 4th task.

---
+++ Mathematical_programming/2d_minimisation/src/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * main.cpp
+ * Fourth task on optimization methods.
+ * Different methods of locating 2d functions minimum.
+ * Vladimir Rutsky <altsysrq@gmail.com>
+ * 29.03.2009
+ */
+
+#ifdef USING_QT_CREATOR
+// Only for debugging in Qt Creator.
+#include </opt/qtcreator-1.0.0/share/qtcreator/gdbmacros/gdbmacros.cpp>
+#endif // USING_QT_CREATOR
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "numeric/numeric.hpp"
+
+int main()
+{
+  typedef numeric::ublas::vector     <double> vector_type;
+  typedef numeric::ublas::unit_vector<double> unit_vector_type;
+  typedef numeric::ublas::matrix     <double> matrix_type;
+  
+  
+  return 0;
+}
