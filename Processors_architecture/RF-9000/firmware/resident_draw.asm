2c41192b968a7fef7179c9c66416bf1cdbc007ac
Vladimir Rutsky <altsysrq@gmail.com>
2009-04-01 09:46:22 +0400

    Added exiting by key press into drawing demo (also Sleep).

--- Processors_architecture/RF-9000/firmware/resident_draw.asm
+++ Processors_architecture/RF-9000/firmware/resident_draw.asm
@@ -7,6 +7,8 @@
     format binary
 
     RealFrameBufferVarAddr = 0x200FEA64
+    RealCheckYesNoKeysFunc = 0x200237A0
+    RealOSSleepFunc        = 0x20090D64
 
 resident_start:
         STMFD   SP!, {R0-R11,LR}
@@ -15,6 +17,7 @@ resident_start:
         ; Loading frame buffer information
         LDR     R5, [PC, frame_buffer_descr_real_addr - $ - 8]
         
+        ; TODO: Use LDMIA R5, {R6, R7}
         LDR     R6, [R5, 0]     ; R6 = width
         LDR     R7, [R0, 4]     ; R7 = height
         
@@ -50,11 +53,29 @@ loop_column:
         BLT     loop_row
         
         ; Checking keys state.
+        STMFD   SP!, {R0-R7}
         
-        ; TODO :)
+        ; Calling `CheckYesNoKeys'.
+        MOV     LR, PC
+        LDR     PC, [PC, RealCheckYesNoKeysFunc_addr - $ - 8]
+        
+        MOV     R9, R0          ; saving result
+        
+        ; Calling `RealOSSleepFunc'.
+        MOV     R0, 1
+        MOV     LR, PC
+        LDR     PC, [PC, RealOSSleepFunc_addr - $ - 8]
+        
+        LDMFD   SP!, {R0-R7}
+        
+        ; Exiting if key was pressed.
+        CMP     R9, 0
+        BNE     resident_end
         
         B      loop
         
 resident_end:
         LDMFD   SP!, {R0-R11,PC}
 frame_buffer_descr_real_addr DW     RealFrameBufferVarAddr
+RealCheckYesNoKeysFunc_addr  DW     RealCheckYesNoKeysFunc
+RealOSSleepFunc_addr         DW     RealOSSleepFunc
