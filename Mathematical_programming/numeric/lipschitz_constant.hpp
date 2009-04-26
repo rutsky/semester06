13ef1c28e22a6e611fa51c23b0ebead1c92cd511
Vladimir Rutsky <altsysrq@gmail.com>
2009-04-26 16:08:22 +0400

    Added comment about probably incorrect piece of code.

--- Mathematical_programming/numeric/lipschitz_constant.hpp
+++ Mathematical_programming/numeric/lipschitz_constant.hpp
@@ -94,7 +94,7 @@ namespace numeric
       }
     }
     
-    scalar_type const resultR = ublas::norm_2(R);
+    scalar_type const resultR = ublas::norm_2(R); // FIXME: I think this is incorrect.
     return resultR;
   }
 } // End of namespace 'numeric'.
