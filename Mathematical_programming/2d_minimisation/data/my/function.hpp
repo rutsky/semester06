f737dba65c74cec8fe4cc2e5282988e2d7449f6d
Vladimir Rutsky <altsysrq@gmail.com>
2009-05-26 02:42:22 +0400

    Added inverse Hessian matrix calculation.

--- Mathematical_programming/2d_minimisation/data/my/function.hpp
+++ Mathematical_programming/2d_minimisation/data/my/function.hpp
@@ -84,4 +84,44 @@ namespace function
     grad(1) = 2.           + 2. / sqrt(2 + x1 * x1 + x2 * x2) * 2 * x2;
     return grad;
   }
+  
+  // Function inverse Hessian matrix.
+  template< class V >
+  ublas::matrix<typename V::value_type> 
+    inverseHessian( V const &x )
+  {
+    BOOST_CONCEPT_ASSERT((ublas::VectorExpressionConcept<V>));
+
+    typedef typename V::value_type     scalar_type;
+    typedef ublas::vector<scalar_type> vector_type;
+    typedef ublas::matrix<scalar_type> matrix_type;
+  
+    BOOST_ASSERT(x.size() == 2);
+    
+    scalar_type const x1 = x(0);
+    scalar_type const x2 = x(1);
+    
+    scalar_type const val = 2 + x1 * x1 + x2 * x2;
+    scalar_type const sqrtval = sqrt(val);
+    
+    // TODO!
+    scalar_type const d2fdx1dx1 = 2. + (4. * sqrtval - (4 * x1 * x1) / sqrtval) / val;
+    scalar_type const d2fdx2dx2 = (4. * sqrtval - (4 * x2 * x2) / sqrtval) / val;
+    scalar_type const d2fdx1dx2 = -4. * x1 * 2 * x2 / val;
+    
+    scalar_type const a = d2fdx1dx1;
+    scalar_type const b = d2fdx1dx2;
+    scalar_type const c = d2fdx1dx2;
+    scalar_type const d = d2fdx2dx2;
+    
+    matrix_type invH(2, 2);
+    invH(0, 0) =  d; invH(0, 1) = -b;
+    invH(1, 0) = -c; invH(1, 1) =  a;
+    
+    scalar_type const denominator = a * d - b * c;
+    BOOST_ASSERT(abs(denominator) > 1e-8);
+    invH *= (1 / denominator);
+
+    return invH;
+  }
 } // End of namespace 'function'
