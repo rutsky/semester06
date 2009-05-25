413850cfce5fe69e7a5e28f87f9a70dff25404ee
Vladimir Rutsky <altsysrq@gmail.com>
2009-05-26 02:42:58 +0400

    Something compilable about Newton method.

--- Mathematical_programming/2d_minimisation/src/main.cpp
+++ Mathematical_programming/2d_minimisation/src/main.cpp
@@ -27,20 +27,21 @@ int main()
   typedef numeric::ublas::unit_vector<scalar_type> unit_vector_type;
   typedef numeric::ublas::matrix     <scalar_type> matrix_type;
   
-  typedef boost::function<scalar_type( vector_type )> function_type;
-  typedef boost::function<vector_type( vector_type )> function_grad_type;
-  //typedef scalar_type (*function_type     )( vector_type const & );
-  //typedef vector_type (*function_grad_type)( vector_type const & );
+  typedef boost::function<scalar_type( vector_type const & )> function_type;
+  typedef boost::function<vector_type( vector_type const & )> function_grad_type;
+  typedef boost::function<matrix_type( vector_type const & )> function_inv_hessian_type;
   typedef scalar_type (*scalar_norm_type  )( scalar_type );
   typedef scalar_type (*vector_norm_type  )( numeric::ublas::vector_expression<vector_type> const & );
     
-  function_type      const f                  = &function::function<vector_type>;
-  function_grad_type const df                 = &function::functionGrad<vector_type>;
-  scalar_type        const preferredPrecision = function::preferredPrecision;
-  scalar_type        const step               = function::step;
-  scalar_norm_type   const sNorm              = &xmath::abs<scalar_type>;
-  vector_norm_type   const vNorm              = &numeric::ublas::norm_2<vector_type>;
+  function_type             const f                  = &function::function<vector_type>;
+  function_grad_type        const df                 = &function::functionGrad<vector_type>;
+  function_inv_hessian_type const invH               = &function::inverseHessian<vector_type>;
+  scalar_type               const preferredPrecision = function::preferredPrecision;
+  scalar_type               const step               = function::step;
+  scalar_norm_type          const sNorm              = &xmath::abs<scalar_type>;
+  vector_norm_type          const vNorm              = &numeric::ublas::norm_2<vector_type>;
   
+  if (0)
   {
     // 
     // Calculating Lipschitz constant.
@@ -76,6 +77,111 @@ int main()
     }
   }
 
+  {
+    //
+    // Newton algorithm.
+    //
+    
+    vector_type startPoint(2);
+    startPoint(0) = function::startX;
+    startPoint(1) = function::startY;
+    
+    std::vector<vector_type> points;
+    
+    vector_type const xMin = numeric::newton::find_min
+                               <function_type, function_grad_type, function_inv_hessian_type, vector_type>(
+                                  f, df, invH,
+                                  startPoint, 
+                                  preferredPrecision, step, 
+                                  std::back_inserter(points));
+    
+    /*
+    {
+      // Saving passed spots.
+      char const *dataFileName = "../output/gd_points.dat";
+      boost::scoped_ptr<std::ofstream> ofs(new std::ofstream(dataFileName));
+      if (ofs->fail())
+      {
+        std::cerr << "Failed to open data file '" << dataFileName << "'.\n";
+        return 1;
+      }
+      
+      std::for_each(points.begin(), points.end(), 
+                    boost::bind<void>(&numeric::output_vector_coordinates<std::ofstream, vector_type>, 
+                                      boost::ref(*ofs), _1, " ", "\n", "%1$15.8f"));
+    }
+    
+    if (points.begin() != points.end())
+    {
+      // Saving passed spots function values (for contours).
+      char const *dataFileName = "../output/gd_contours.gp";
+      boost::scoped_ptr<std::ofstream> ofs(new std::ofstream(dataFileName));
+      if (ofs->fail())
+      {
+        std::cerr << "Failed to open data file '" << dataFileName << "'.\n";
+        return 1;
+      }
+      
+      *ofs << "set cntrparam levels discrete ";
+      
+      std::transform(++points.begin(), points.end(), std::ostream_iterator<double>(*ofs, ","), f);
+      *ofs << f(*points.begin());
+      *ofs << std::endl;
+    }
+    
+    {
+      // Generating report data.
+      
+      char const *dataFileName = "../output/gd_result.tex";
+      boost::scoped_ptr<std::ofstream> ofs(new std::ofstream(dataFileName));
+      if (ofs->fail())
+      {
+        std::cerr << "Failed to open data file '" << dataFileName << "'.\n";
+        return 1;
+      }
+      
+      boost::optional<scalar_type> prevFMinVal;
+      for (size_t p = 0; p < numeric::array_size(function::precisions); ++p)
+      {
+        scalar_type const precision = function::precisions[p];
+        
+        std::vector<vector_type> points;
+        vector_type xMin;
+        numeric::gradient_descent::gradient_descent_result const result =
+            numeric::gradient_descent::find_min
+                                   <function_type, function_grad_type, vector_type>(
+                                      f, df, 
+                                      startPoint, 
+                                      precision, step, 
+                                      xMin,
+                                      numeric::true_predicate(), std::back_inserter(points));
+        BOOST_ASSERT(result == result); // TODO: Handle result states.
+
+        *ofs << boost::format("%1$1.0e") % precision << " & " << points.size() << " & (";
+        numeric::output_vector_coordinates(*ofs, xMin, ", ", "");
+        *ofs << ") & ";
+        
+        scalar_type const curFMinVal = f(xMin);
+        
+        *ofs << boost::format("%1$15.8f") % curFMinVal << " & ";
+        
+        if (prevFMinVal)
+          *ofs << boost::format("%1e") % (curFMinVal - *prevFMinVal);
+        
+        prevFMinVal = curFMinVal;
+        
+        vector_type const curGrad = df(xMin);
+        *ofs << " & (";
+        numeric::output_vector_coordinates(*ofs, curGrad, ", ", "", "%1e");
+        *ofs << ")";
+
+        *ofs << " \\\\\n";
+      }
+    }
+    */
+  }
+  
+  if (0)
   {
     //
     // Gradient descent algorithm.
@@ -182,6 +288,7 @@ int main()
     }
   }
   
+  if (0)
   {
     //
     // Genetic algorithm.
