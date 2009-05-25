3fa663d3d19badca25db0e98e7c64f665dd683f3
Vladimir Rutsky <altsysrq@gmail.com>
2009-05-25 05:50:15 +0400

    Fixed identation.

--- Mathematical_programming/numeric/matrix_ops.hpp
+++ Mathematical_programming/numeric/matrix_ops.hpp
@@ -23,8 +23,8 @@ namespace numeric
   template< class MatrixType, class ColsForwardIterator >
   inline
   matrix_submatrix<MatrixType> submatrixi( MatrixType &data, 
-                                          size_t              rowsStart, size_t              rowsSize,
-                                          ColsForwardIterator colsBegin, ColsForwardIterator colsEnd )
+                                           size_t              rowsStart, size_t              rowsSize,
+                                           ColsForwardIterator colsBegin, ColsForwardIterator colsEnd )
   {
     basic_range<size_t, long> range(rowsStart, rowsSize);
     return matrix_submatrix<MatrixType>(data, range.begin(), range.end(), colsBegin, colsEnd);
@@ -33,8 +33,8 @@ namespace numeric
   template< class MatrixType, class RowsForwardIterator >
   inline
   matrix_submatrix<MatrixType const> submatrixi( MatrixType const &data, 
-                                                RowsForwardIterator rowsBegin, RowsForwardIterator rowsEnd,
-                                                size_t              colsStart, size_t              colsSize )
+                                                 RowsForwardIterator rowsBegin, RowsForwardIterator rowsEnd,
+                                                 size_t              colsStart, size_t              colsSize )
   {
     basic_range<size_t, long> range(colsStart, colsSize);
     return matrix_submatrix<MatrixType const>(data, rowsBegin, rowsEnd, range.begin(), range.end());
