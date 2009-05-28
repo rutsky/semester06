27820d3d4747bf14baf98de6d65703a800c0552c
Vladimir Rutsky <altsysrq@gmail.com>
2009-05-28 05:29:52 +0400

    Added comment on constraints output format.

--- Mathematical_programming/final_test/compute_lines_equations.m
+++ Mathematical_programming/final_test/compute_lines_equations.m
@@ -9,6 +9,7 @@ y = [0 6 8  4 0 0];
 pInsideX = 1;
 pInsideY = 1;
 
+printf("LP problem constraints:\n");
 for i = 1:(size(x)(2) - 1)
   # Line equation: (x - x1) / (x1 - x2) = (y - y1) / (y1 - y2)
   # In common form: (y1 - y2) * x + (x2 - x1) * y + (x1 * y2 - x2 * y1) = 0
@@ -24,5 +25,6 @@ for i = 1:(size(x)(2) - 1)
   
   s = sign(A * pInsideX + B * pInsideY + C);
   
+  # Outputting in form of LP problem constraints.
   printf("%3g %3g  (%+g)  %3g\n", A, B, s, -C);
 endfor
