662ddf884cca6c30a8f64eb8f2e9242564441a7e
Vladimir Rutsky <altsysrq@gmail.com>
2009-06-04 15:38:20 +0400

    Improved code style.

--- Processors_architecture/PCX/src/mtimer.hpp
+++ Processors_architecture/PCX/src/mtimer.hpp
@@ -22,10 +22,10 @@ public:
     QueryPerformanceFrequency((LARGE_INTEGER *)&ticks_per_sec);
   }
 
-  ticks_type GetTicks( ticks_type *ticks = 0 )
+  ticks_type GetTicks( ticks_type *ticks = 0 ) const
   {
     ticks_type now_ticks;
-    BOOL state = QueryPerformanceCounter((LARGE_INTEGER *)&now_ticks);
+    BOOL const state = QueryPerformanceCounter((LARGE_INTEGER *)&now_ticks);
     assert(state == TRUE);
 
     if (ticks)
@@ -34,10 +34,10 @@ public:
     return now_ticks;
   }
 
-  // Returns number of second since some event
-  double GetTime( double *time = 0 )
+  // Returns number of second since some event.
+  double GetTime( double *time = 0 ) const
   {
-    double time_stamp = static_cast<double>(GetTicks()) / ticks_per_sec;
+    double const time_stamp = static_cast<double>(GetTicks()) / ticks_per_sec;
 
     if (time != 0)
       *time = time_stamp;
