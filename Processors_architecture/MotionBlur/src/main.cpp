9b268994f606efca1fa4d2e89414d82fc905ed2f
Vladimir Rutsky <altsysrq@gmail.com>
2009-06-07 23:31:18 +0400

    Added frame counter and task switching.

--- Processors_architecture/MotionBlur/src/main.cpp
+++ Processors_architecture/MotionBlur/src/main.cpp
@@ -19,6 +19,18 @@
 
 #include "effect_motion_blur.h"
 
+// Useful `array_size' function.
+#ifdef WIN32
+  #define array_size(x) \
+    (sizeof(x) / sizeof((x)[0]))
+#else // WIN32
+  template< typename T, size_t N >
+  size_t array_size( T (&)[N] ) 
+  { 
+    return N; 
+  }
+#endif // not WIN32
+
 namespace constants
 {
   char   const *defaultBackgroundImagePath = "../data/Kimi_Raikkonen_won_2007_Brazil_GP_scaled.jpg";
@@ -29,6 +41,15 @@ namespace constants
   int const windowWidth  = 1000;
   int const windowHeight = 500;
   int const windowBPP    = 32;
+  
+  Uint32 const fpsUpdateInterval = 1000; // in ticks (1/1000 of second).
+  
+  char const *effectImplsNames[] = 
+    {
+      "Base version",
+      "Paired copying",
+    };
+  size_t const nEffectImpls = array_size(effectImplsNames);
 } // End of namespace `constants'.
 
 namespace cmdline
@@ -78,43 +99,87 @@ namespace cmdline
   }
 } // End of namespace `cmdline'.
 
-// Usefull function.
-#ifdef WIN32
-  #define array_size(x) \
-    (sizeof(x) / sizeof((x)[0]))
-#else // WIN32
-  template< typename T, size_t N >
-  size_t array_size( T (&)[N] ) 
-  { 
-    return N; 
-  }
-#endif // not WIN32
+void updateWindowTitle( int effectCurImpl, double fps )
+{
+  std::ostringstream ostr;
+  
+  assert(effectCurImpl < static_cast<int>(array_size(constants::effectImplsNames)));
+  ostr << constants::effectImplsNames[effectCurImpl] << " " << std::fixed << std::setw(9) << std::setprecision(4) <<  fps;
+  
+  SDL_WM_SetCaption(ostr.str().c_str(), NULL);
+}
 
 // Main program loop.
 int mainLoop( SDL_Surface *screen )
 {
+  size_t frameCounter(0);
+  Uint32 frameCounterLastUpdateTicks = SDL_GetTicks();
+
+  int curEffectImpl = 0;
+  double curEffectFPS = 0;
+  
+  updateWindowTitle(curEffectImpl, curEffectFPS);
+    
   bool exit = false;
   while (!exit)
   {
-    SDL_Event event;
-
     // Polling all events.
-    while (SDL_PollEvent(&event))
     {
-      switch (event.type)
+      SDL_Event event;
+      while (SDL_PollEvent(&event))
       {
-        case SDL_KEYDOWN:
-          std::cout << "The " << SDL_GetKeyName(event.key.keysym.sym) << " key was pressed!" << std::endl;
-          break;
-            
-        case SDL_QUIT:
-          // Exiting.
-          exit = true;
-          break;
+        switch (event.type)
+        {
+          case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_LEFT)
+            {
+              curEffectImpl = (curEffectImpl + 1) % constants::nEffectImpls;
+
+              frameCounterLastUpdateTicks = SDL_GetTicks();
+              curEffectFPS = 0;
+              frameCounter = 0;
+              updateWindowTitle(curEffectImpl, curEffectFPS);
+            }
+            else if (event.key.keysym.sym == SDLK_RIGHT)
+            {
+              curEffectImpl = (curEffectImpl + constants::nEffectImpls - 1) % constants::nEffectImpls;
+
+              frameCounterLastUpdateTicks = SDL_GetTicks();
+              curEffectFPS = 0;
+              frameCounter = 0;
+              updateWindowTitle(curEffectImpl, curEffectFPS);
+            }
+            // std::cout << "The " << SDL_GetKeyName(event.key.keysym.sym) << " key was pressed!" << std::endl; // TODO: remove this line.
+            break;
+              
+          case SDL_QUIT:
+            // Exiting.
+            exit = true;
+            break;
+        }
       }
     }
     
     // Doing work.
+    {
+      SDL_Delay(10);
+    }
+    
+    // Timing.
+    {
+      ++frameCounter;
+      
+      Uint32 const curTicks = SDL_GetTicks();
+      if (curTicks >= frameCounterLastUpdateTicks + constants::fpsUpdateInterval)
+      {
+        Uint32 const ticksDelta = curTicks - frameCounterLastUpdateTicks;
+        
+        frameCounterLastUpdateTicks = curTicks;
+        curEffectFPS = static_cast<double>(frameCounter) * 1000.0 / ticksDelta;
+        frameCounter = 0;
+        updateWindowTitle(curEffectImpl, curEffectFPS);
+      }
+    }
   }
   
   return 0;
