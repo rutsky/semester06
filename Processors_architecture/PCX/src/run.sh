3943d1384687add5964c9a18f7fe05d433b1554f
Vladimir Rutsky <altsysrq@gmail.com>
2009-06-07 19:48:24 +0400

    Added script for generating running results log.

---
+++ Processors_architecture/PCX/src/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+./pcx_decode ../data/baboon.pcx     ../data/baboon.pnm     10 10 > baboon.log
+./pcx_decode ../data/baboon4.pcx    ../data/baboon4.pnm    10 10 > baboon4.log
+./pcx_decode ../data/square_2x2.pcx ../data/square_2x2.pnm 10 10 > square_2x2.log
