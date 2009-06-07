e7f7f0b5d4f8b9503d8f291eb9c52dff23ef993d
Vladimir Rutsky <altsysrq@gmail.com>
2009-06-07 20:16:29 +0400

    Updated run parameters.

--- Processors_architecture/PCX/src/run.bat
+++ Processors_architecture/PCX/src/run.bat
@@ -1,3 +1,3 @@
-pcx_decode.exe ../data/baboon.pcx     ../data/baboon.pnm     10 10 > baboon.log
-pcx_decode.exe ../data/baboon4.pcx    ../data/baboon4.pnm    10 10 > baboon4.log
-pcx_decode.exe ../data/square_2x2.pcx ../data/square_2x2.pnm 10 10 > square_2x2.log
+pcx_decode.exe ../data/baboon.pcx     ../data/baboon.pnm     100 10 > baboon.log
+pcx_decode.exe ../data/baboon4.pcx    ../data/baboon4.pnm    100 10 > baboon4.log
+pcx_decode.exe ../data/square_2x2.pcx ../data/square_2x2.pnm 100 10 > square_2x2.log
