ed6cd32514ac1d1b11b6541b5963c577a702d91a
Vladimir Rutsky <altsysrq@gmail.com>
2009-06-09 15:17:41 +0400

    Fixed run script for Windows.

--- Processors_architecture/MotionBlur/src/run.bat
+++ Processors_architecture/MotionBlur/src/run.bat
@@ -1,3 +1,1 @@
-#!/bin/sh
-
-Debug/motion_blur.exe ../data/Kimi_Raikkonen_won_2007_Brazil_GP_scaled.jpg ../data/Kimi_Raikkonen_won_2007_Brazil_GP_scaled_car.png 30 -2
+Debug\MotionBlur.exe ../data/Kimi_Raikkonen_won_2007_Brazil_GP_scaled.jpg ../data/Kimi_Raikkonen_won_2007_Brazil_GP_scaled_car.png 30 -2
